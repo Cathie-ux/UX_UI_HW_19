e049529f1fe3693c080a1f0dc8799acefc9e7e6c
shorty2cat <73954900+shorty2cat@users.noreply.github.com>
2021-05-05 16:23:26 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(function() {
+    $("#AboutMe").slideDown(1000).delay(1000).slideUp(1000).slideDown(1000);
+
+
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementsByClassName("close")[0];
+// btn.onclick = function() {
+//     modal.style.display = "block";
+//   }
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+//   window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+// }
+
+});
