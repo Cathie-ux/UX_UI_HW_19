3898b0ab22de9cad4b70c39834d16b673eae6b73
shorty2cat <73954900+shorty2cat@users.noreply.github.com>
2021-05-08 10:14:37 -0400

    linked

--- js/index.js
+++ js/index.js
@@ -4,19 +4,21 @@ $(function() {
     $("#AboutMe").slideDown(1000).delay(1000).slideUp(1000).slideDown(1000);
 
 
-// var modal = document.getElementById("myModal");
-// var btn = document.getElementById("myBtn");
-// var span = document.getElementsByClassName("close")[0];
-// btn.onclick = function() {
-//     modal.style.display = "block";
-//   }
-//   span.onclick = function() {
-//     modal.style.display = "none";
-//   }
-//   window.onclick = function(event) {
-//     if (event.target == modal) {
-//       modal.style.display = "none";
-//     }
-// }
+var modal = document.getElementById("myModal");
+var btn = document.getElementsByClassName("ContactModal")[0];
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+
 
 });
